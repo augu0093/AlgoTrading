65190022f61c8ce6fbf255d606dc8a9f3acba057
augustsemrau <augu0093@gmail.com>
2020-09-29 18:00:48 +0200

    Will use NLTK

---
+++ twitterSentiment/vader.py
@@ -0,0 +1,20 @@
+"""
+First try at sentiment analysis using NLTK
+@augustsemrau
+"""
+
+
+
+
+class Sentiment:
+    """Class used for analyzing sentiment in tweets"""
+    def __init__(self):
+        self
+
+
+
+
+
+
+
+

b332d3bd2d185da744ad8b65de198a8e16679411
augustsemrau <augu0093@gmail.com>
2020-09-30 08:23:32 +0200

    Testing Vader capabilities

--- twitterSentiment/vader.py
+++ twitterSentiment/vader.py
@@ -3,13 +3,44 @@ First try at sentiment analysis using NLTK
 @augustsemrau
 """
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
-
-class Sentiment:
+class VaderSentiment:
     """Class used for analyzing sentiment in tweets"""
     def __init__(self):
-        self
+        self.analyser = SentimentIntensityAnalyzer()
+
+
+
+    def sentiment_analyzer_scores(self, sentence):
+        score = self.analyser.polarity_scores(sentence)
+        print("Sentence: '{:}' - Scores: {}".format(sentence, str(score)))
+        return score
+
+
+
+
+
+
+
+
+
+
+
+"""
+Testing Vader workings
+"""
+if __name__ == "__main__":
+
+    # Initialize Vader model class
+    Vader = VaderSentiment()
+
+    # Scoring a sentence to test
+    score = Vader.sentiment_analyzer_scores("The phone is cool :)")
+    print("Compound score: ", score['compound'])
+
+
 
 
 
